ad6b5c910976feb2aae8c753a57f6b07f121c892
Cyberspace27 <ariel.villarreal26@gmail.com>
2021-03-15 02:15:18 -0600

    User chromedriver for ubuntu

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -19,8 +19,11 @@ public class LoginTest {
 	LoginPage login ;
   @BeforeTest
   public void setUp() {
-	  projectPath =  System.getProperty("user.dir");
-	  System.setProperty("webdriver.chrome.driver", projectPath+"/drivers/chromedriver.exe");
+//	  projectPath =  System.getProperty("user.dir");
+//	  System.setProperty("webdriver.chrome.driver", projectPath+"/drivers/chromedriver.exe");
+	
+	  System.setProperty("webdriver.chrome.driver", "/usr/bin/google-chrome");
+		
 	  driver = new ChromeDriver();
 	  driver.manage().window().maximize();
 	  driver.get("https://www.google.com/");
