6685bbfd5447e9ba12f26af4883dbbc999c053e4
Cyberspace27 <ariel.villarreal26@gmail.com>
2021-03-15 21:08:14 -0600

    resolve webdriver issue test

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -1,42 +1,39 @@
 package tests;
 
 import org.testng.annotations.Test;
-
 import pages.LoginPage;
-
 import org.testng.annotations.BeforeTest;
-
 import java.util.concurrent.TimeUnit;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterTest;
 
 public class LoginTest {
+	
 	//MISSING BASE_TEST
 	WebDriver driver = null ;
 	String projectPath = "";
-	LoginPage login ;
+	LoginPage login = null;
+	
   @BeforeTest
   public void setUp() {
-	//  projectPath =  System.getProperty("user.dir");
-	//  System.setProperty("webdriver.chrome.driver", projectPath+"/drivers/chromedriver.exe");
+	  
+	  projectPath =  System.getProperty("user.dir");
+	  System.setProperty("webdriver.chrome.driver", projectPath+"/drivers/chromedriverL");
 	 
-		
-	  System.setProperty("webdriver.chrome.driver", "./drivers/bin/chromedriver");
-		
+//	  System.setProperty("webdriver.chrome.driver", "/usr/bin/google-chrome");	
 	  driver = new ChromeDriver();
 	  driver.manage().window().maximize();
 	  driver.get("https://www.google.com/");
 	  driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
 	  login = new LoginPage(driver);
   }
+
   @Test
   public void verifyGoogleSearch() {
 	  login.search("Hola Done...!!");
   }
   
-
   @AfterTest
   public void tearDown() {
 	  driver.close();
